c58b634f7998fa665ebadba5fbfe809d51023170
st128282 <slapina094@gmail.com>
2024-09-27 04:13:51 +0400

    Выполнил задачу-1 без условия чтения из файлов

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
-
-int main() {
-  int a = 678;
-  int b = 655;
-  std::cout << a+b << std::endl;
-         
-  return 0;
+#include <cmath>
+using namespace std;
+int main()
+{
+  int a = 13;
+  int b = 43;
+  if ((a < b ) && (b < 250000)){
+    for(int i {a}; i <= b; i++)
+    {
+      int c = 0;
+      for(int j {2}; int(j <= sqrt(i)); j++)
+      {
+        if (i % j == 0)
+        {
+          c = c + 1;
+          break;
+        }
+      }
+      if (c == 0)
+      {
+        std::cout << i << std::endl;
+      }
+    }
+  }
 }
