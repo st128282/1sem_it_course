27f9be386772b41abfb0b150616bb51252a38322
Шляпин Илья <142336050+isshlyapin@users.noreply.github.com>
2024-10-14 14:24:45 +0300

    Update prime_num.cpp

--- task1/prime_num.cpp
+++ task1/prime_num.cpp
@@ -2,37 +2,35 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-bool prime_num(int a)
-{
-  for(int i {2}; i <= sqrt(a); i++)
-  {
-    if (a % i == 0)
-    {
+
+bool isPrimeNum(int a) {
+  for(int i {2}; i <= sqrt(a); i++) {
+    if (a % i == 0) {
       return false;
     }
   }
   return true;
 }
-void prime_num_betweenA_B(int a, int b, std::vector<int> &referenceprimeA_B)
-{
-  if ((a > 0) & (b > a) & (b < 250000))
-  {
-    for(int i {a}; i <= b; i++)
-    {
-      if (prime_num(i) & (i != 1))
-      {
-        referenceprimeA_B.push_back(i);
-      }
+
+void getPrimeNums(int a, int b, std::vector<int> &primeNums) {
+  if (!((a > 0) && (b > a) && (b < 250000))) {
+    return;
+  }
+  
+  for(int i {a}; i <= b; i++) {
+    if (isPrimeNum(i)) {
+      primeNums.push_back(i);
     }
   }
 }
-void recordInFile(std::vector<int> &referenceprimeA_B)
-{
+
+void recordInFile(const std::vector<int> &primeNums) {
   std::ofstream fout;
   fout.open("out.txt");
-  for (int i {0}; i < referenceprimeA_B.size(); i++)
-  {
-    fout<< referenceprimeA_B[i] <<std::endl;
+  
+  for (int x : primeNums) {
+    fout << x << std::endl;
   }
+  
   fout.close();
 }
